585168eee408cc71a3d2c963b0cdc89359fad615
IgnOry <iory@ucm.es>
2022-01-29 13:47:29 +0100

    Code and Scene refactor

--- Global Game Jam 22/Assets/Scripts/Gameplay/Board.cs
+++ Global Game Jam 22/Assets/Scripts/Gameplay/Board.cs
@@ -13,67 +13,14 @@ public class Board : MonoBehaviour
     public GameObject whiteBoardObjectPrefab;
     public GameObject blackBoardObjectPrefab;
     // Start is called before the first frame update
-    void Start()
-    {
-        board = new bool[size, size];
 
-        boardGameObjects = new GameObject[size, size];
-        int aux = 0;
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-            {
-                if (aux % 2 == 0)
-                    boardGameObjects[i, j] = Instantiate(whiteBoardObjectPrefab);
-                else
-                    boardGameObjects[i, j] = Instantiate(blackBoardObjectPrefab);
 
-                boardGameObjects[i, j].transform.position = new Vector3(i, 0, j);
-                aux++;
-            }
-        }
-    }
+    
 
-    public void SpawnEnemies ()
-    {
-        for (int i = 0; i < enemies.Length; i++)
-        {
-            Enemy en = enemies[i].GetComponent<Enemy>();
-            int xPos = Random.Range(0, size);
-
-            if (en.enemyClass == Class.Pawn)
-            {
-                while (board[xPos, 1])
-                    xPos = Random.Range(0, size);
-
-                en.position = new KeyValuePair < int, int>(xPos, 1); //Always in second row (0 based)
-            }
-            else
-            {
-                while (board[xPos, 0])
-                    xPos = Random.Range(0, size);
-
-                en.position = new KeyValuePair<int, int>(xPos, 1); //Always in second row (0 based)
-            }
-
-            board[en.position.Key, en.position.Value] = true;
-
-            Vector3 pos = boardGameObjects[en.position.Key, en.position.Value].transform.position;
-            pos.y += 0.75f;
-            enemies[i].transform.position = pos;
-        }
-    }
-
-    public void SpawnPlayer()
-    {
-        int xPos = Random.Range(0, size);
-
-        player.GetComponent<Character>().position = new KeyValuePair<int, int>(xPos, size-1);
-        board[xPos, size-1] = true;
-    }
+    
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("a");
+        //Debug.Log("a");
     }
 }
--- Global Game Jam 22/Assets/Scripts/Gameplay/Room.cs
+++ Global Game Jam 22/Assets/Scripts/Gameplay/Room.cs
@@ -4,41 +4,122 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    public Board board;
+    public bool[,] board;
     public GameObject player;
+
+    public Transform BoardParent;
+    public Transform EnemiesParent;
+    
     int enemies;
     int roomNumber = 1;
+
+    public int size;
+    public GameObject[,] boardGameObjects;
+    public GameObject[] enemiesArray;
+    public GameObject whiteBoardObjectPrefab;
+    public GameObject blackBoardObjectPrefab;
 
     [SerializeField]
     GameObject pawn;
     [SerializeField]
     GameObject bishop;
     // Start is called before the first frame update
-    void Start()
+    public void SetUpRoom()
     {
+        BoardParent = transform.GetChild(0);
+        EnemiesParent = transform.GetChild(1);
+            
         enemies = roomNumber + 1;
-        board = GetComponent<Board>();
-        board.player = player;
+        SetupBoard();
 
-        board.enemies = new GameObject[enemies];
+        enemiesArray = new GameObject[enemies];
         
         GameObject _en;
         Enemy en;
 
-        for (int i = 0; i < board.enemies.Length; i++)
+        for (int i = 0; i < enemiesArray.Length; i++)
         {
             _en = Instantiate(pawn);// new GameObject("Enemy");
             en = _en.AddComponent<Enemy>();
-            board.enemies[i] = _en;
+            enemiesArray[i] = _en;
         }
 
-        board.SpawnEnemies();
-        board.SpawnPlayer();
+        SpawnEnemies();
+        SpawnPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetupBoard()
+    {
+        board = new bool[size, size];
+
+        boardGameObjects = new GameObject[size, size];
+        int aux = 0;
+
+        GameObject auxGO;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                
+                if (aux % 2 == 0)
+                {
+                    auxGO = Instantiate(whiteBoardObjectPrefab);
+                }
+                else
+                {
+                    auxGO = Instantiate(blackBoardObjectPrefab);
+                }
+
+                boardGameObjects[i, j] = auxGO;
+                boardGameObjects[i, j].transform.parent = BoardParent;
+                boardGameObjects[i, j].transform.position = new Vector3(i, 0, j);
+                aux++;
+            }
+        }
+    }
+
+    public void SpawnEnemies()
+    {
+        for (int i = 0; i < enemiesArray.Length; i++)
+        {
+            Enemy en = enemiesArray[i].GetComponent<Enemy>();
+            int xPos = Random.Range(0, size);
+
+            if (en.enemyClass == EnemyClass.Pawn)
+            {
+                while (board[xPos, 1])
+                    xPos = Random.Range(0, size);
+
+                en.position = new KeyValuePair<int, int>(xPos, 1); //Always in second row (0 based)
+            }
+            else
+            {
+                while (board[xPos, 0])
+                    xPos = Random.Range(0, size);
+
+                en.position = new KeyValuePair<int, int>(xPos, 1); //Always in second row (0 based)
+            }
+
+            board[en.position.Key, en.position.Value] = true;
+
+            Vector3 pos = boardGameObjects[en.position.Key, en.position.Value].transform.position;
+            pos.y += 0.75f;
+            enemiesArray[i].transform.position = pos;
+            enemiesArray[i].transform.parent = EnemiesParent;
+        }
+    }
+    public void SpawnPlayer()
+    {
+        int xPos = Random.Range(0, size);
+
+        player.GetComponent<Character>().position = new KeyValuePair<int, int>(xPos, size - 1);
+        board[xPos, size - 1] = true;
     }
 }
