aca09f0e2559c7da20d119377a27991385bbe6f2
al385748 <al385748@uji.es>
2022-01-29 01:36:37 +0100

    Particles

---
+++ Global Game Jam 22/Assets/CamRotation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotation : MonoBehaviour
+{
+    public Transform target;
+    public float speedMod = 1.0f;
+
+    void Update()
+    {
+        target = GameObject.Find("Pawn(Clone)").transform;
+        transform.RotateAround(target.position, new Vector3(0.0f, 1.0f, 0.0f), 20 * Time.deltaTime * speedMod);
+
+    }
+}
